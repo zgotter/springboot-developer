9135e49af27a815b1c45e4a8c8aea33062698d85
zgotter <shkim4738@gmail.com>
2024-12-25 21:33:05 +0900

    Configuring entity - refactoring with lombok

--- src/main/java/me/sounghan/springbootdeveloper/domain/Article.java
+++ src/main/java/me/sounghan/springbootdeveloper/domain/Article.java
@@ -1,9 +1,14 @@
 package me.sounghan.springbootdeveloper.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity // 엔티티로 지정
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
 
     @Id
@@ -21,20 +26,5 @@ public class Article {
     public Article(String title, String content) {
         this.title = title;
         this.content = content;
-    }
-
-    protected Article() {} // 기본 생성자
-
-    // getters
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getContent() {
-        return content;
     }
 }
