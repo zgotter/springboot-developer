971db1fd6b97b69dd19ecc6b3ec31ff1f8ba46ac
zgotter <shkim4738@gmail.com>
2024-12-25 21:31:34 +0900

    Configuring entity - Article

---
+++ src/main/java/me/sounghan/springbootdeveloper/domain/Article.java
@@ -0,0 +1,40 @@
+package me.sounghan.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+
+@Entity // 엔티티로 지정
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키를 자동으로 1씩 증가
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder // 빌더 패턴으로 객체 생성
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    protected Article() {} // 기본 생성자
+
+    // getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
