20c2850c995b10cd4df148fe32020450618ffc58
zgotter <shkim4738@gmail.com>
2025-01-24 00:16:38 +0900

    Ch8.6 Impl logout

--- src/main/java/me/sounghan/springbootdeveloper/controller/UserApiController.java
+++ src/main/java/me/sounghan/springbootdeveloper/controller/UserApiController.java
@@ -1,9 +1,14 @@
 package me.sounghan.springbootdeveloper.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import me.sounghan.springbootdeveloper.dto.AddUserRequest;
 import me.sounghan.springbootdeveloper.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -16,5 +21,18 @@ public class UserApiController {
     public String signup(AddUserRequest request) {
         userService.save(request); // 회원가입 메서드 호출
         return "redirect:/login"; // 회원 가입이 완료된 이후에 로그인 페이지로 이동
+    }
+
+    @GetMapping("/logout")
+    public String logout(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+        new SecurityContextLogoutHandler().logout(
+                request,
+                response,
+                SecurityContextHolder.getContext().getAuthentication()
+        );
+        return "redirect:/login";
     }
 }
