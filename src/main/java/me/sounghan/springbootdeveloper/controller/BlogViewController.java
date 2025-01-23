36f60f71e384ac6f7a3af4ad1c6e8a483757e868
zgotter <shkim4738@gmail.com>
2025-01-23 23:56:36 +0900

    Ch7.3 Impl blog post view

--- src/main/java/me/sounghan/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/sounghan/springbootdeveloper/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package me.sounghan.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.sounghan.springbootdeveloper.domain.Article;
 import me.sounghan.springbootdeveloper.dto.ArticleListViewResponse;
+import me.sounghan.springbootdeveloper.dto.ArticleViewResponse;
 import me.sounghan.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -24,5 +27,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
---
+++ src/main/java/me/sounghan/springbootdeveloper/dto/ArticleViewResponse.java
@@ -0,0 +1,24 @@
+package me.sounghan.springbootdeveloper.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.sounghan.springbootdeveloper.domain.Article;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+public class ArticleViewResponse {
+
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public ArticleViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+        this.createdAt = article.getCreatedAt();
+    }
+}
